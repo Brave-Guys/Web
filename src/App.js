040743091d2590b3e97f5c463893185acf90b018
combikms <combikms@naver.com>
2025-04-08 07:19:29 +0900

    :art: Style: 상단 헤더 메뉴 디자인

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Header from './components/HeaderMenu.js';
+import './App.css';
 
 function App() {
   return (
--- src/components/HeaderMenu.js
+++ src/components/HeaderMenu.js
@@ -1,8 +1,11 @@
+import '../styles/HeaderMenu.css'
+import logo from '../assets/logo.png';
 
 const Header = () => {
     return (
-        <div>
-            <p>asdfgdgd</p>
+        <div className='nav'>
+            <img src={logo} alt="StrengthHub" className="nav-logo" />
+            <h2 className='nav-logo-text'>StrengthHub</h2>
         </div>
     )
 }
