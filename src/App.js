bf2feae314bc15f6aa1e09d2ae8d244ed80151fc
DESKTOP-SRSMOI5\USER <dhdheb@naver.com>
2025-04-12 18:06:17 +0900

    "로그인 화면"

--- src/App.js
+++ src/App.js
@@ -3,15 +3,17 @@ import './App.css';
 import Register from './pages/register.js'
 import PageTitle from './components/PageTitle.js';
 import { Routes, Route } from 'react-router-dom';
+import Login from './pages/login.js'
 
 function App() {
   return (
     <div>
       <Header />
-      <div className='content'>
+      <div>
 
         <Routes>
           <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
 
       </div>
---
+++ src/pages/login.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import './login.css'; // 스타일 분리 :)
+import PageTitle from '../components/PageTitle';
+import bgImage from '../assets/텅키.jpg';
+
+const Login = () => {
+    return (
+        <div
+            className="login-page"
+            style={{
+                backgroundImage: `url(${bgImage})`,
+                backgroundSize: 'cover',
+                backgroundPosition: 'top center',
+                backgroundRepeat: 'no-repeat',
+                backgroundAttachment: 'fixed',
+            }}
+        >
+            <div className="login-container">
+                <div className="login-left">
+                    <div className="login-text">
+                        운동 기록을<br />한눈에 확인하세요
+                    </div>
+                </div>
+
+                <div className="login-right">
+                    <div className="login-box">
+                        <h2>로그인</h2>
+                        <input type="text" placeholder="ID" />
+                        <input type="password" placeholder="비밀번호" />
+
+                        <div className="login-buttons">
+                            <button className="signup-btn">회원가입</button>
+                            <button className="login-btn">로그인</button>
+                        </div>
+
+                        <div className="social-buttons">
+                            <button className="kakao">카카오로 로그인</button>
+                            <button className="naver">네이버로 로그인</button>
+                            <button className="google">구글로 로그인</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
