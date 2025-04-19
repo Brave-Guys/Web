e1d1144a823c21d0f52a9899f810f566622421a2
combikms <combikms@naver.com>
2025-04-19 16:20:17 +0900

    :sparkles: Feat: 로그아웃

--- src/components/HeaderMenu.js
+++ src/components/HeaderMenu.js
@@ -1,13 +1,26 @@
-import '../styles/HeaderMenu.css'
+import '../styles/HeaderMenu.css';
 import logo from '../assets/logo.png';
+import { useNavigate } from 'react-router-dom';
 
 const Header = () => {
+    const navigate = useNavigate();
+
+    const handleLogout = () => {
+        localStorage.removeItem('token'); // 토큰 삭제
+        navigate('/login'); // 로그인 페이지로 이동
+    };
+
     return (
         <div className='nav'>
-            <img src={logo} alt="StrengthHub" className="nav-logo" />
-            <h2 className='nav-logo-text'>StrengthHub</h2>
+            <div className="nav-left">
+                <img src={logo} alt="StrengthHub" className="nav-logo" />
+                <h2 className='nav-logo-text'>StrengthHub</h2>
+            </div>
+            <button className="logout-btn" onClick={handleLogout}>
+                로그아웃
+            </button>
         </div>
-    )
-}
+    );
+};
 
 export default Header;
