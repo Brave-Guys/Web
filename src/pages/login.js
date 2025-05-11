e435aaa493564787fb1b5d6ce647f046b97daf5f
Inseok Kang <combikms@naver.com>
2025-05-11 12:17:01 +0900

    :art: Style: 로그인 배경 3개 페이드 트랜지션

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,7 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import '../styles/login.css';
-import bgImage from '../assets/welcome.png';
 import { loginUser } from '../apis/loginUser';
 import CustomButton from '../components/CustomButton';
 import InputField from '../components/InputField';
@@ -12,6 +11,13 @@ import googleIcon from '../assets/google.png';
 const Login = () => {
     const [loginData, setLoginData] = useState({ username: '', password: '' });
     const [errorMessage, setErrorMessage] = useState('');
+    const [bgIndex, setBgIndex] = useState(0);
+
+    const backgroundImages = [
+        require('../assets/bg1.png'),
+        require('../assets/bg2.png'),
+        require('../assets/bg3.png'),
+    ];
 
     const navigate = useNavigate();
 
@@ -28,106 +34,108 @@ const Login = () => {
         }
     };
 
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setBgIndex((prev) => (prev + 1) % backgroundImages.length);
+        }, 3000);
+        return () => clearInterval(interval);
+    }, []);
+
     return (
-        <div
-            className="login-page"
-            style={{
-                backgroundImage: `url(${bgImage})`,
-                backgroundSize: 'cover',
-                backgroundPosition: 'top center',
-                backgroundRepeat: 'no-repeat',
-                backgroundAttachment: 'fixed',
-                height: '100vh',
-            }}
-        >
-            <div className="login-container">
-                <div className="login-left">
-                    <div className="login-text">
-                        운동 기록을<br />한눈에<br />확인하세요
+        <div className="login-background">
+            {backgroundImages.map((img, i) => (
+                <div
+                    key={i}
+                    className={`bg-slide ${i === bgIndex ? 'active' : ''}`}
+                    style={{ backgroundImage: `url(${img})` }}
+                />
+            ))}
+
+            <div className="login-page">
+                <div className="login-container">
+                    <div className="login-left">
                     </div>
-                </div>
 
-                <div className="login-right">
-                    <div className="login-box">
-                        <div className="login-title-row" style={{ padding: '0px' }}>
-                            <div style={{ fontSize: '30px', fontWeight: 'bold' }}>로그인</div>
-
-                        </div>
-                        <div className="error-message">{errorMessage === '' ? '\u00A0' : errorMessage}</div>
-
-
-                        <div style={{ margin: '20px' }}></div>
-
-
-                        <div className="login-box-inner">
-                            <InputField
-                                label="ID"
-                                guide=""
-                                placeholder=""
-                                value={loginData.username}
-                                onChange={handleChange('username')}
-                                status="default"
-                            />
-
-                            <InputField
-                                label="비밀번호"
-                                guide=""
-                                placeholder=""
-                                type="password"
-                                value={loginData.password}
-                                onChange={handleChange('password')}
-                                status="default"
-                            />
-
-                            <div className="login-buttons">
-                                <CustomButton
-                                    label="회원가입"
-                                    size="large"
-                                    color="gray"
-                                    onClick={() => navigate('/register')}
-                                />
-                                <CustomButton
-                                    label="로그인"
-                                    size="large"
-                                    color="gray"
-                                    onClick={handleLogin}
-                                />
+                    <div className="login-right">
+                        <div className="login-box">
+                            <div className="login-title-row" style={{ padding: '0px' }}>
+                                <div style={{ fontSize: '30px', fontWeight: 'bold' }}>로그인</div>
                             </div>
-                        </div>
-                        <div style={{ marginTop: '20px' }}>
-                            <div className="social-buttons">
-                                <button className="social-button kakao">
-                                    <div className="social-content">
-                                        <div className="icon-area">
-                                            <img src={kakaoIcon} alt="Kakao" className="social-icon" />
-                                        </div>
-                                        <div className="text-area">
-                                            카카오톡 계정으로 로그인
-                                        </div>
-                                    </div>
-                                </button>
+                            <div className="error-message">{errorMessage === '' ? '\u00A0' : errorMessage}</div>
 
-                                <button className="social-button naver">
-                                    <div className="social-content">
-                                        <div className="icon-area">
-                                            <img src={naverIcon} alt="Naver" className="social-icon" />
-                                        </div>
-                                        <div className="text-area">
-                                            네이버 계정으로 로그인
-                                        </div>
-                                    </div>
-                                </button>
+                            <div style={{ margin: '20px' }}></div>
 
-                                <button className="social-button google">
-                                    <div className="social-content">
-                                        <div className="icon-area">
-                                            <img src={googleIcon} alt="Google" className="social-icon" />
+                            <div className="login-box-inner">
+                                <InputField
+                                    label="ID"
+                                    guide=""
+                                    placeholder=""
+                                    value={loginData.username}
+                                    onChange={handleChange('username')}
+                                    status="default"
+                                />
+
+                                <InputField
+                                    label="비밀번호"
+                                    guide=""
+                                    placeholder=""
+                                    type="password"
+                                    value={loginData.password}
+                                    onChange={handleChange('password')}
+                                    status="default"
+                                />
+
+                                <div className="login-buttons">
+                                    <CustomButton
+                                        label="회원가입"
+                                        size="large"
+                                        color="gray"
+                                        onClick={() => navigate('/register')}
+                                    />
+                                    <CustomButton
+                                        label="로그인"
+                                        size="large"
+                                        color="gray"
+                                        onClick={handleLogin}
+                                    />
+                                </div>
+                            </div>
+
+                            <div style={{ marginTop: '20px' }}>
+                                <div className="social-buttons">
+                                    <button className="social-button kakao">
+                                        <div className="social-content">
+                                            <div className="icon-area">
+                                                <img src={kakaoIcon} alt="Kakao" className="social-icon" />
+                                            </div>
+                                            <div className="text-area">
+                                                카카오톡 계정으로 로그인
+                                            </div>
                                         </div>
-                                        <div className="text-area">
-                                            구글 계정으로 로그인
+                                    </button>
+
+                                    <button className="social-button naver">
+                                        <div className="social-content">
+                                            <div className="icon-area">
+                                                <img src={naverIcon} alt="Naver" className="social-icon" />
+                                            </div>
+                                            <div className="text-area">
+                                                네이버 계정으로 로그인
+                                            </div>
                                         </div>
-                                    </div>
-                                </button>
+                                    </button>
+
+                                    <button className="social-button google">
+                                        <div className="social-content">
+                                            <div className="icon-area">
+                                                <img src={googleIcon} alt="Google" className="social-icon" />
+                                            </div>
+                                            <div className="text-area">
+                                                구글 계정으로 로그인
+                                            </div>
+                                        </div>
+                                    </button>
+                                </div>
                             </div>
                         </div>
                     </div>
