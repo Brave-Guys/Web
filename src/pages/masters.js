cfd452f8a2dd6009cebfc7a7288ad344b733a2e4
combikms <combikms@naver.com>
2025-05-17 16:08:44 +0900

    :sparkles: Feat: 상급자 목록

--- src/pages/masters.js
+++ src/pages/masters.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/Masters.css';
 import PageTitle from '../components/PageTitle';
+import axios from 'axios';
 
 const Masters = () => {
+    const [seniorUsers, setSeniorUsers] = useState([]);
+
+    useEffect(() => {
+        const fetchSeniors = async () => {
+            try {
+                const token = localStorage.getItem('token');
+                const res = await axios.get(`${process.env.REACT_APP_API_URL}/users/seniors`, {
+                    headers: {
+                        Authorization: `Bearer ${token}`
+                    }
+                });
+                setSeniorUsers(res.data);
+            } catch (err) {
+                console.error('상급자 정보 불러오기 실패:', err);
+            }
+        };
+        fetchSeniors();
+    }, []);
+
     return (
         <div className="masterpage-wrapper">
             <div className="title-row">
@@ -15,6 +35,19 @@ const Masters = () => {
                 <Link to="/how-to-be-master" className="help-link">
                     상급자가 되려면 어떻게 해야 하나요?
                 </Link>
+            </div>
+
+            <div className="senior-user-list">
+                {seniorUsers.length === 0 ? (
+                    <p>등록된 상급자가 없습니다.</p>
+                ) : (
+                    seniorUsers.map((user) => (
+                        <div key={user.id} className="senior-user-card">
+                            <p><strong>{user.name}</strong></p>
+                            <p>{user.email}</p>
+                        </div>
+                    ))
+                )}
             </div>
         </div>
     );
