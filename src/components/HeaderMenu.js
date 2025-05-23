186fcff09323bb27a6e2b3799532cb2828f178d4
Inseok Kang <combikms@naver.com>
2025-05-24 03:17:01 +0900

    :art: Style: 바로가기 아이콘들 좌측으로 이동

--- src/components/HeaderMenu.js
+++ src/components/HeaderMenu.js
@@ -21,44 +21,49 @@ const Header = () => {
 
     return (
         <div className='nav'>
-            <div className="nav-left" onClick={() => navigate('/main')}>
-                <img src={logo} alt="StrengthHub" className="nav-logo" />
-                <h2 className='nav-logo-text'>StrengthHub</h2>
+            <div className="nav-left">
+                <div className="logo-wrapper" onClick={() => navigate('/main')}>
+                    <img src={logo} alt="StrengthHub" className="nav-logo" />
+                    <h2 className='nav-logo-text'>StrengthHub</h2>
+                </div>
+
+                <div className="nav-links">
+                    <div className="icon-btn" onClick={() => navigate('/weekly-workout')}>
+                        <FaVideo className="icon" />
+                        <span>금주의 운동</span>
+                    </div>
+                    <div className="icon-btn" onClick={() => navigate('/share-plus')}>
+                        <FaShareAlt className="icon" />
+                        <span>Share+</span>
+                    </div>
+                    <div className="icon-btn" onClick={() => navigate('/exercise-tip')}>
+                        <FaBookOpen className="icon" />
+                        <span>운동 설명서</span>
+                    </div>
+                    <div className="icon-btn" onClick={() => navigate('/board')}>
+                        <FaComments className="icon" />
+                        <span>커뮤니티</span>
+                    </div>
+                    <div className="icon-btn" onClick={() => navigate('/workoutlog')}>
+                        <FaClipboardList className="icon" />
+                        <span>운동 기록</span>
+                    </div>
+                    <div className="icon-btn" onClick={() => navigate('/masters')}>
+                        <FaCrown className="icon" />
+                        <span>상급자</span>
+                    </div>
+                    <div className="icon-btn" onClick={() => navigate('/challenges')}>
+                        <FaTrophy className="icon" />
+                        <span>챌린지</span>
+                    </div>
+                    <div className="icon-btn" onClick={() => navigate('/inquiry')}>
+                        <FaQuestionCircle className="icon" />
+                        <span>문의하기</span>
+                    </div>
+                </div>
             </div>
 
-            <div className="nav-icons">
-                <div className="icon-btn" onClick={() => navigate('/weekly-workout')}>
-                    <FaVideo className="icon" />
-                    <span>금주의 운동</span>
-                </div>
-                <div className="icon-btn" onClick={() => navigate('/share-plus')}>
-                    <FaShareAlt className="icon" />
-                    <span>Share+</span>
-                </div>
-                <div className="icon-btn" onClick={() => navigate('/exercise-tip')}>
-                    <FaBookOpen className="icon" />
-                    <span>운동 설명서</span>
-                </div>
-                <div className="icon-btn" onClick={() => navigate('/board')}>
-                    <FaComments className="icon" />
-                    <span>커뮤니티</span>
-                </div>
-                <div className="icon-btn" onClick={() => navigate('/workoutlog')}>
-                    <FaClipboardList className="icon" />
-                    <span>운동 기록</span>
-                </div>
-                <div className="icon-btn" onClick={() => navigate('/masters')}>
-                    <FaCrown className="icon" />
-                    <span>상급자</span>
-                </div>
-                <div className="icon-btn" onClick={() => navigate('/challenges')}>
-                    <FaTrophy className="icon" />
-                    <span>챌린지</span>
-                </div>
-                <div className="icon-btn" onClick={() => navigate('/inquiry')}>
-                    <FaQuestionCircle className="icon" />
-                    <span>문의</span>
-                </div>
+            <div className="nav-right">
                 <div className="icon-btn" onClick={() => navigate(user ? '/mypage' : '/login')}>
                     {user?.imgUrl ? (
                         <img src={user.imgUrl} alt="프로필" className="profile-image" />
