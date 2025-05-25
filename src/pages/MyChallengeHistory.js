a8f5a33d03819ca4d3ebe076913513ea66f89765
combikms <combikms@naver.com>
2025-05-25 16:23:34 +0900

    :art: Style: 그지같은 요소들 다 제거

--- src/pages/MyChallengeHistory.js
+++ src/pages/MyChallengeHistory.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import PageTitle from '../components/PageTitle';
 import { getMyChallengeParticipants } from '../apis/getMyChallengeParticipants';
-import '../styles/ChallengeBoard.css';
+import styles from '../styles/MyChallengeHistory.module.css';
 
 const MyChallengeHistory = () => {
     const [challenges, setChallenges] = useState([]);
@@ -23,29 +23,24 @@ const MyChallengeHistory = () => {
     }, []);
 
     return (
-        <div className="challenge-board">
-            <div className="challenge-header">
+        <div className={styles['my-challenge-wrapper']}>
+            <div className={styles['challenge-header']}>
                 <PageTitle
                     title="완료한 챌린지"
                     showBackArrow={true}
                     description="내가 수행한 챌린지들이에요."
                 />
             </div>
-            <div className="challenge-card-list">
+            <div className={styles['challenge-card-list']}>
                 {challenges.map((challenge) => (
                     <div
                         key={challenge.id}
-                        className="challenge-card"
+                        className={styles['challenge-card']}
                         onClick={() => navigate(`/challenges/${challenge.challengeId}/participants/${challenge.id}`)}
                     >
-                        <img
-                            src={challenge.thumbnailUrl || 'https://via.placeholder.com/120x80'}
-                            alt="thumbnail"
-                            className="challenge-thumbnail"
-                        />
-                        <div className="challenge-info">
-                            <h4 className="challenge-title">{challenge.content}</h4>
-                            <p className="challenge-date">{new Date(challenge.writeDate).toLocaleDateString()}</p>
+                        <div className={styles['challenge-info']}>
+                            <h4 className={styles['challenge-title']}>{challenge.content}</h4>
+                            <p className={styles['challenge-date']}>{new Date(challenge.writeDate).toLocaleDateString()}</p>
                         </div>
                     </div>
                 ))}
