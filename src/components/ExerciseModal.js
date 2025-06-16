c9c457c79263e2dc4c326f46d3a5b3a4c212554a
DESKTOP-SRSMOI5\USER <dhdheb@naver.com>
2025-06-17 03:01:07 +0900

    "modal"

--- src/components/ExerciseModal.js
+++ src/components/ExerciseModal.js
@@ -10,12 +10,6 @@ const ExerciseModal = ({ exercise, onClose }) => {
             <div className="modal-content" onClick={(e) => e.stopPropagation()}>
                 <button className="close-button" onClick={onClose}>✖</button>
                 <div className="modal-left">
-                    {/* exercise.image 가 있다면 이미지를 보여줌 */}
-                    {/* {exercise.image ? (
-                        <img src={tempImg} alt={exercise.name} className="exercise-image" />
-                    ) : (
-                        <div className="exercise-image-placeholder">이미지 없음</div>
-                    )} */}
                     <img src={tempImg} alt={exercise.name}/>
                 </div>
                 <div className="modal-right">
